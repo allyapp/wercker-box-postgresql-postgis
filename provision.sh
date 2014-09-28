84a9f189fc8ccf98724b667dde195db0b8fabd86
Cristian Bica <cristian.bica@gmail.com>
2014-09-28 22:05:42 +0300

    fix installer

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -c | awk '{print $2}'`-pgdg main" >/etc/apt/sources.list.d/pgdg.list
-sudo aptitude install -y postgresql-9.3 postgresql-9.3-postgis-2.1
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo aptitude install -y  postgresql-9.3 postgresql-9.3-postgis-2.1
 echo "wercker" | passwd postgres --stdin
 PGPASSWORD=werckerdb1 createdb -U postgres
