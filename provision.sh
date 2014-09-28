9798804fa6df62a60f80e04ac5c9ea8fa9fa3a07
Cristian Bica <cristian.bica@gmail.com>
2014-09-28 22:21:31 +0300

    Fix provisioning script

--- provision.sh
+++ provision.sh
@@ -2,6 +2,7 @@
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo aptitude update -y
 sudo aptitude install -y  postgresql-9.3 postgresql-9.3-postgis-2.1
-echo "wercker" | passwd postgres --stdin
+echo "postgres:wercker" | sudo chpasswd
 PGPASSWORD=werckerdb1 createdb -U postgres
