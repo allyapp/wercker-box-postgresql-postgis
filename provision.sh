1cee9690994057e48514bc325a21d7e3d7840716
Cristian Bica <cristian.bica@gmail.com>
2014-10-14 22:21:58 +0300

    Change PSQL postgres password

--- provision.sh
+++ provision.sh
@@ -5,7 +5,9 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs
 sudo aptitude update -y
 sudo aptitude install -y  postgresql-9.3 postgresql-9.3-postgis-2.1
 echo "postgres:wercker" | sudo chpasswd
-sudo su postgres - -c 'echo "CREATE DATABASE werckerdb1;" | psql'
+sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE werckerdb1;"
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'wercker';"
+
 sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/*/main/postgresql.conf
 echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
 echo "host    all             all             ::1/128               md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
