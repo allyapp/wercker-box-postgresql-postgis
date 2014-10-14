95ace3937539fb15508a90abdf61ff37f34028c8
Cristian Bica <cristian.bica@gmail.com>
2014-10-14 21:51:41 +0300

    Make pg listen on * and allow 0/0

--- provision.sh
+++ provision.sh
@@ -6,3 +6,7 @@ sudo aptitude update -y
 sudo aptitude install -y  postgresql-9.3 postgresql-9.3-postgis-2.1
 echo "postgres:wercker" | sudo chpasswd
 sudo su postgres - -c 'echo "CREATE DATABASE werckerdb1;" | psql'
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/*/main/postgresql.conf
+echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
+echo "host    all             all             ::1/128               md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
+sudo service postgresql restart
