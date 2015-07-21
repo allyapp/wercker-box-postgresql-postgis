cc236b8f2ad0b9342573ca1d8d2b6d3ec2fd27a7
Klaus Hartl <klaus@allyapp.com>
2015-07-21 12:21:49 +0200

    Postgres 9.4

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 sudo aptitude update -y
-sudo aptitude install -y  postgresql-9.3 postgresql-9.3-postgis-2.1
+sudo aptitude install -y  postgresql-9.4 postgresql-9.4-postgis-2.1
 echo "postgres:wercker" | sudo chpasswd
 sudo su postgres - -c 'echo "CREATE DATABASE werckerdb1;" | psql'
 sudo su postgres - -c 'echo "ALTER USER postgres WITH PASSWORD '"'"'wercker'"'"';" | psql'
